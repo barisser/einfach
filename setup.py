34de7da8c447846ca525e64bda8d174cb668b842
Andrew Barisser <barisser@protonmail.com>
2017-09-04 16:59:21 +0200

    Added gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='einfach',
+      version='0.0.1',
+      author='Andrew Barisser',
+      author_email='barisser@protonmail.com',
+      license='MIT',
+      packages=['einfach']
+      )
