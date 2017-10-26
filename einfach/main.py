d03d189dd301b9fec6d125976d17951635e9ef08
Andrew Barisser <barisser@protonmail.com>
2017-10-25 23:00:01 -0700

    silhouette scores

--- einfach/main.py
+++ einfach/main.py
@@ -4,6 +4,12 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model
+from keras import backend as K
+
 
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
@@ -12,26 +18,38 @@ encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
 
 
 def make_autoencoder():
-    input_img = Input(shape=(784,))
-    # "encoded" is the encoded representation of the input
-    encoded = Dense(encoding_dim, activation='relu')(input_img)    
+    input_img = Input(shape=(28, 28, 1))
 
-    # "decoded" is the lossy reconstruction of the input
-    decoded = Dense(784, activation='sigmoid')(encoded)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    # this model maps an input to its reconstruction
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
+    x = UpSampling2D((2, 2))(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
     autoencoder = Model(input_img, decoded)
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
     # this model maps an input to its encoded representation
     encoder = Model(input_img, encoded)
-    encoded_input = Input(shape=(encoding_dim,))
-    decoder = Model(encoded_input, autoencoder.layers[-1](encoded_input))
+    #import pdb;pdb.set_trace()
+    #encoded_input = encoded#Input(shape=(encoding_dim,))
+    decoder = None# Model(encoded_input, autoencoder.layers[-1](encoded_input))
     autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
     return autoencoder, encoder, decoder
@@ -41,20 +59,24 @@ def make_autoencoder():
 autoencoder, encoder, decoder = make_autoencoder()
 
 autoencoder.fit(x_train, x_train,
-                    epochs=20,
+                    epochs=10,
                     batch_size=256,
                     shuffle=True,
                     validation_data=(x_test, x_test))
 
 encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+#decoded_imgs = decoder.predict(encoded_imgs)
+
+encoded_imgs = np.reshape(encoded_imgs, [-1, 128])
 
 scores = []
-for i in range(1, 20):
-    kmeans = KMeans(n_clusters=i).fit(encoded_imgs)
-    score = kmeans.score(encoded_imgs)
+for i in range(2, 20):
+    kmeans = KMeans(n_clusters=i)
+    labels = kmeans.fit_predict(encoded_imgs)
+    score = silhouette_score(encoded_imgs, labels)
+    score2 = kmeans.score(encoded_imgs)
     print "{0} -- {1}".format(i, score)
-    scores.append(score)
+    scores.append([score, score2])
 
 plt.plot(scores)
 plt.show()
